9e6e1c3bedc8845df4acd5a15de887541e126637
IHadAFish <798532768@qq.com>
2018-04-24 20:47:51 +0800

    Premissions.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+
     print("Logged in as")
     print(client.user.name)
     print(client.user.id)
@@ -18,15 +19,28 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    if message.content.startswith("!logout"):
-        await client.logout()
-        print("Exiting.")
-        try:
-            sys.exit()
-        except:
-            pass
+    premissions = message.author.premissions
 
-    elif message.content.startswith("!test"):
+    for premission_name, have_premission in premissions:
+        if premission_name == "administrator":
+            admin = have_premission
+            break
+
+    if admin:
+
+        # For testing only.
+        if message.content.startswith("!eval"):
+            eval(message.content[6:])
+
+        if message.content.startswith("!logout"):
+            await client.logout()
+            print("Exiting.")
+            try:
+                sys.exit()
+            except:
+                pass
+
+    if message.content.startswith("!test"):
         await client.send_message(message.channel, "Reporting in.")
 
 @client.event
@@ -45,9 +59,12 @@ async def on_member_join(member):
 
 @client.event
 async def randPlay():
+
     if not gmtime()[3] % 2:
+
         plays = ["with himself", "with your mind", "with gravity", "dead", "tricks"]
         playing = randSample(plays, 1)[0]
+        
         await client.change_presence(game=discord.Game(name=playing))
 
 client.run(credentials.getToken())
